a2fa7f1c366f370c9971363a7e31406e2b313856
maryblack <maniya.chernova@gmail.com>
2019-07-22 14:05:45 +0300

    accepted 2/3 GBC

--- hw11_2.py
+++ hw11_2.py
@@ -1,13 +1,12 @@
-import inline as inline
 import math
-import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import ensemble
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
-% matplotlib
-inline
+import numpy as np
+
 
 
 def train_test_split_xy(df):
@@ -25,6 +24,7 @@ def plot_log(test_loss, train_loss):
     plt.plot(test_loss, 'r', linewidth=2)
     plt.plot(train_loss, 'g', linewidth=2)
     plt.legend(['test', 'train'])
+    plt.show()
 
 
 def GB_clf(X_train, X_test, y_train, y_test, lr):
@@ -32,11 +32,31 @@ def GB_clf(X_train, X_test, y_train, y_test, lr):
     clf.fit(X_train, y_train)
     y_pred_train = clf.predict_proba(X_train)
     y_pred_test = clf.predict_proba(X_test)
-    # pred = quality_pred(clf.staged_decision_function(X_test))
-    test_loss = log_loss(y_test, y_pred_test)
-    train_loss = log_loss(y_train, y_pred_train)
-    plot_log(test_loss, train_loss)
-    # print(f'pred {pred}')
+    clf.staged_decision_function(X_test)
+
+    min_log = 100
+    min_i = 0
+    test_deviance = []
+    for i, y_pred_test in enumerate(clf.staged_decision_function(X_test)):
+        y_pred = [quality_pred(el) for el in y_pred_test]
+        test_log = log_loss(y_test, y_pred)
+        if test_log<min_log:
+            min_log = test_log
+            min_i = i
+        test_deviance.append(test_log)
+
+    train_deviance = []
+    for i, y_pred_train in enumerate(clf.staged_decision_function(X_train)):
+        y_pred = [quality_pred(el) for el in y_pred_train]
+        train_log = log_loss(y_train, y_pred)
+        train_deviance.append(train_log)
+    print(f'minimum: {min_i}  {min_log}')
+    #plt.plot(test_deviance, linewidth=2, label='learning_rate: {lr}')
+    #plt.plot(train_deviance, linewidth=2, , label=label)
+
+
+
+
 
 
 def main():
@@ -45,7 +65,15 @@ def main():
     learning_rate = [1, 0.5, 0.3, 0.2, 0.1]
     X_train, X_test, y_train, y_test = train_test_split_xy(df)
     # print(X_train, X_test, y_train, y_test)
-    GB_clf(X_train, X_test, y_train, y_test, learning_rate[0])
+    # plt.figure()
+    # for rate in learning_rate:
+    #     GB_clf(X_train, X_test, y_train, y_test, rate)
+    # plt.show()
+
+    plt.figure()
+    GB_clf(X_train, X_test, y_train, y_test, 0.2)#36  0.53
+    plt.show()
+
 
 
 if __name__ == '__main__':
