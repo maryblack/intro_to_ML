88b66f7a9de81e624ee4655a98374f9ce1d77042
maryblack <maniya.chernova@gmail.com>
2019-07-22 14:21:35 +0300

    accepted GradientBoostingClf

--- hw11_2.py
+++ hw11_2.py
@@ -1,13 +1,10 @@
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import ensemble
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
-import numpy as np
-
-
 
 def train_test_split_xy(df):
     X = df.iloc[:, 1:len(df.columns)].to_numpy()
@@ -19,44 +16,35 @@ def quality_pred(pred):
     return 1 / (1 + math.exp(-pred))
 
 
-def plot_log(test_loss, train_loss):
-    plt.figure()
-    plt.plot(test_loss, 'r', linewidth=2)
-    plt.plot(train_loss, 'g', linewidth=2)
-    plt.legend(['test', 'train'])
-    plt.show()
+def dev_log(clf, X, y):
+    min_log = 100
+    min_i = 0
+    deviance = []
+    for i, y_test in enumerate(clf.staged_decision_function(X)):
+        y_pred = [quality_pred(el) for el in y_test]
+        test_log = log_loss(y, y_pred)
+        if test_log < min_log:
+            min_log = test_log
+            min_i = i
+        deviance.append(test_log)
+    return min_log, min_i, deviance
 
 
 def GB_clf(X_train, X_test, y_train, y_test, lr):
     clf = GradientBoostingClassifier(learning_rate=lr, n_estimators=250, verbose=True, random_state=241)
     clf.fit(X_train, y_train)
-    y_pred_train = clf.predict_proba(X_train)
-    y_pred_test = clf.predict_proba(X_test)
-    clf.staged_decision_function(X_test)
-
-    min_log = 100
-    min_i = 0
-    test_deviance = []
-    for i, y_pred_test in enumerate(clf.staged_decision_function(X_test)):
-        y_pred = [quality_pred(el) for el in y_pred_test]
-        test_log = log_loss(y_test, y_pred)
-        if test_log<min_log:
-            min_log = test_log
-            min_i = i
-        test_deviance.append(test_log)
-
-    train_deviance = []
-    for i, y_pred_train in enumerate(clf.staged_decision_function(X_train)):
-        y_pred = [quality_pred(el) for el in y_pred_train]
-        train_log = log_loss(y_train, y_pred)
-        train_deviance.append(train_log)
-    print(f'minimum: {min_i}  {min_log}')
+    test_min_log, test_min_i, test_deviance = dev_log(clf, X_test, y_test)
+    train_min_log, train_min_i, train_deviance = dev_log(clf, X_train, y_train)
+    print(f'minimum test: {test_min_log} {test_min_i}')
+    print(f'minimum train: {train_min_log} {train_min_i}')
     #plt.plot(test_deviance, linewidth=2, label='learning_rate: {lr}')
-    #plt.plot(train_deviance, linewidth=2, , label=label)
+    #plt.plot(train_deviance, linewidth=2)
 
-
-
-
+def RF_clf(X_train, X_test, y_train, y_test):
+    clf = RandomForestClassifier(n_estimators=36, random_state=241)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict_proba(X_test)
+    print(f'RFC log__loss: {log_loss(y_test,y_pred)}')
 
 
 def main():
@@ -70,10 +58,8 @@ def main():
     #     GB_clf(X_train, X_test, y_train, y_test, rate)
     # plt.show()
 
-    plt.figure()
-    GB_clf(X_train, X_test, y_train, y_test, 0.2)#36  0.53
-    plt.show()
-
+    GB_clf(X_train, X_test, y_train, y_test, 0.2)#0.53 36
+    RF_clf(X_train, X_test, y_train, y_test)#0.54
 
 
 if __name__ == '__main__':
