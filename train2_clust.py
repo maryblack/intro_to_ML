2a4a5210a6b6b95a431e698a2ea851d61875b2be
maryblack <maniya.chernova@gmail.com>
2019-07-25 23:01:30 +0700

    working colour reduction

--- train2_clust.py
+++ train2_clust.py
@@ -1,9 +1,25 @@
-from skimage.io import imread
 import skimage as sk
+from skimage.io import imread
 import pylab
 import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
+import statistics as st
+
+
+class Pixel:
+    def __init__(self, pixel, rgb, cluster):
+        self.pix = pixel
+        self.rgb = rgb
+        self.clust = cluster
+
+    # def mean_rgb(self):
+    #     self.rgb = mean_clust(self.clust)
+    #
+    # def median_rgb(self):
+    #     self.rgb = median_clust(self.clust)
+
+
 
 def RGB_matrix(image):
     y = []
@@ -35,28 +51,68 @@ def clust(X):
     return kmeans.labels_
 
 def by_cluster(list_of_clust, X):
-    num = max(list_of_clust)
-    lists = [[] for i in range(1,num)]
+    num = max(list_of_clust)+1
+    lists = [[] for i in range(0, num)]
     i = 0
     for i in range(0, len(X)):
         ind = list_of_clust[i]
         lists[ind].append(X[i])
-    return lists
+    return num, lists
 
+def mean_rgb(cluster, lists):
+    init = lists[cluster]
+    R = []
+    G = []
+    B = []
+    for el in init:
+        R.append(el[0])
+        G.append(el[1])
+        B.append(el[2])
+    R_mean = st.mean(R)
+    G_mean = st.mean(G)
+    B_mean = st.mean(B)
+    M = np.array([R_mean, G_mean, B_mean])
+    return M
 
+def list_mean_clust(list_of_cluster, lists):
+    mean_values = []
+    for i in range(0, len(lists)):
+        mv = mean_rgb(i, lists)
+        mean_values.append(mv)
+    return mean_values
 
+def reduction(X,list_of_cluster, mean_values):
+    for i in range (0, len(list_of_cluster)):
+        cl = list_of_cluster[i]
+        X[i] = mean_values[cl]
+
+    return X
+
+#
+# def median_rgb(cluster):
+
+def colour_reduction(image):
+    pylab.imshow(image)
+    pylab.show()
+    X,y,w,h = RGB_matrix(image)
+    list_of_clust = clust(X)
+    num, lists = by_cluster(list_of_clust, X)
+    mean_values = list_mean_clust(list_of_clust, lists)
+    low_colours = reduction(X, list_of_clust, mean_values)
+    I = mtx_to_img(low_colours, y, w, h)
+    pylab.imshow(I)
+    pylab.show()
 
 
 def main():
     image = imread('parrots.jpg')
+    #F = sk.img_as_float(image)
+    colour_reduction(image)
 
-    X,y,w,h = RGB_matrix(image)
-    I = mtx_to_img(X,y,w, h)
-    F = sk.img_as_float(image)
-    print(max(clust(X)))
-
-    pylab.imshow(I)
-    pylab.show()
+    #I = mtx_to_img(X,y,w, h)
+    #F = sk.img_as_float(image)
+    # pylab.imshow(I)
+    # pylab.show()
 
 if __name__ == '__main__':
     main()
